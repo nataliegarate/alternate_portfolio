e6458131dd4988059cedba97705fc0784138d028
Natalie Garate <nataliegarate@Natalies-MacBook-Air.local>
2020-01-14 19:42:46 -0800

    setup react router and deleted initial un-needed files

---
+++ src/Components/aboutMe.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class AboutMe extends Component {
+  render() {
+    return <h1>AboutMe</h1>;
+  }
+}
+
+export default AboutMe;
---
+++ src/Components/landingPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class LandingPage extends Component {
+  render() {
+    return <h1>LandingPage</h1>;
+  }
+}
+
+export default LandingPage;
