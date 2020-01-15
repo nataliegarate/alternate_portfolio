8d44ec2955729a63a7046326afb4b52e74dcea9e
nataliegarate <natalieegarate@hotmail.com>
2020-01-14 21:41:13 -0800

    basic working landing page

--- src/Components/landingPage.js
+++ src/Components/landingPage.js
@@ -1,8 +1,49 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class LandingPage extends Component {
   render() {
-    return <h1>LandingPage</h1>;
+    return (
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              className="avatar-img"
+              src="https://cdn.pixabay.com/photo/2014/04/02/17/07/user-307993_1280.png"
+            />
+            <div className="banner-text">
+              <h1>Fullstack Developer</h1>
+              <hr />
+              <p>
+                HTML/CSS | Javascript | React | Redux | Node.js | Express |
+                Sequelize | PostgreSQL | Learning Java
+              </p>
+              <div className="social-links">
+                <a
+                  href="https://www.linkedin.com/in/natalie-garate/"
+                  target="_blank"
+                >
+                  {" "}
+                  <i
+                    className="fa fa-linkedin-square"
+                    rel="noopener noreferrer"
+                    aria-hidden="true"
+                  />{" "}
+                </a>
+                <a href="https://github.com/nataliegarate" target="_blank">
+                  {" "}
+                  <i
+                    className="fa fa-github-square"
+                    rel="noopener noreferrer"
+                    aria-hidden="true"
+                  />{" "}
+                </a>
+              </div>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    );
   }
 }
 
