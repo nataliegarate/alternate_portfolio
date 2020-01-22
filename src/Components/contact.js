5266e2bd54726c6b575520317d029a783ed3cf3f
nataliegarate <natalieegarate@hotmail.com>
2020-01-22 15:30:36 -0800

    added links to contact page

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -28,27 +28,39 @@ class Contact extends Component {
             <div>
               <List className="contact-list">
                 <center>
-                <ListItem>
-                  <ListItemContent className="contact-text">
-                    <i className="fa fa-envelope" aria-hidden="true" />
-                    <span className="contact-detail">
-                      natalieegarate@hotmail.com
-                    </span>
-                  </ListItemContent>
-                </ListItem>
+                  <ListItem>
+                    <ListItemContent className="contact-text">
+                      <a
+                        style={{ textDecoration: "none", color: "black" }}
+                        href="mailto:natalieegarate@hotmail.com?Subject=Hello%20again"
+                        target="_blank"
+                      >
+                        <i className="fa fa-envelope" aria-hidden="true" />
+                        <span className="contact-detail">
+                          natalieegarate@hotmail.com
+                        </span>
+                      </a>
+                    </ListItemContent>
+                  </ListItem>
 
-                <ListItem>
-                  <ListItemContent className="contact-text">
-                    <i
-                      className="fa fa-linkedin"
-                      aria-hidden="true"
-                      rel="noopener noreferrer"
-                    />
-                    <span className="contact-detail">
-                      linkedin.com/in/natalie-garate
-                    </span>
-                  </ListItemContent>
-                </ListItem>
+                  <ListItem>
+                    <ListItemContent className="contact-text">
+                      <a
+                        href="https:/www.linkedin.com/in/natalie-garate"
+                        style={{ textDecoration: "none", color: "black" }}
+                        target="_blank"
+                      >
+                        <i
+                          className="fa fa-linkedin"
+                          aria-hidden="true"
+                          rel="noopener noreferrer"
+                        />
+                        <span className="contact-detail">
+                          linkedin.com/in/natalie-garate
+                        </span>
+                      </a>
+                    </ListItemContent>
+                  </ListItem>
                 </center>
               </List>
             </div>
