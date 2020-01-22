5173ef4964e5690c8f0ea849b11842529edf378f
nataliegarate <natalieegarate@hotmail.com>
2020-01-21 16:28:47 -0800

    removed title filler and added resume links

--- src/Components/main.js
+++ src/Components/main.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import LandingPage from "./landingPage";
-import AboutMe from "./aboutMe";
 import Projects from "./projects";
 import Resume from "./resume";
 import Contact from "./contact";
@@ -9,7 +8,6 @@ import Contact from "./contact";
 const Main = () => (
   <Switch>
     <Route exact path="/" component={LandingPage} />
-    <Route exact path="/aboutme" component={AboutMe} />
     <Route exact path="/projects" component={Projects} />
     <Route exact path="/resume" component={Resume} />
     <Route exact path="/contact" component={Contact} />
